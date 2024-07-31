c66799433ba57ff6b6ba44f2578b74a812ea1e29
Naduladisanayaka494 <it20247140@my.sliit.lk>
2024-07-31 13:37:12 +0530

    login interface and routes created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SingnupComponent } from './auth/components/singnup/singnup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SingnupComponent
   ],
   imports: [
     BrowserModule,
