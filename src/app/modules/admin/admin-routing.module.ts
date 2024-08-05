82384b4596980ea36583710eb17fabf8885b05b4
Naduladisanayaka494 <it20247140@my.sliit.lk>
2024-08-05 12:17:14 +0530

    modules routing created

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { AddMoneyComponent } from './add-money/add-money.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-
+  { path: 'transactions', component: TransactionsComponent },
+  { path: 'add-money', component: AddMoneyComponent}
 ];
 
 @NgModule({
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddMoneyComponent } from './add-money/add-money.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    AddMoneyComponent,
+    TransactionsComponent
   ],
   imports: [
     CommonModule,
