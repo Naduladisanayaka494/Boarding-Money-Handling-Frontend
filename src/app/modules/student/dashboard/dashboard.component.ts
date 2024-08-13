6675904fdd413b8322eacaab7c1d2fedde372e77
Naduladisanayaka494 <it20247140@my.sliit.lk>
2024-08-13 16:17:43 +0530

    dashboard created

--- src/app/modules/student/dashboard/dashboard.component.ts
+++ src/app/modules/student/dashboard/dashboard.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/services/student/student.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  recentTransactions: any[] = [];
+  currentBalance: number = 0;
 
-  constructor() { }
+  constructor(private studentService: StudentService) {}
 
   ngOnInit(): void {
+    this.fetchRecentTransactions();
   }
 
+  fetchRecentTransactions(): void {
+    this.studentService.getMoneyTransactions().subscribe(
+      (data) => {
+        // Sort transactions by date in descending order and get the 5 most recent
+        const sortedTransactions = data.sort(
+          (a: { transactionDateTime: string | number | Date; }, b: { transactionDateTime: string | number | Date; }) =>
+            new Date(b.transactionDateTime).getTime() -
+            new Date(a.transactionDateTime).getTime()
+        );
+        this.recentTransactions = sortedTransactions.slice(0, 5);
+
+        // Calculate the current balance
+        this.currentBalance = this.calculateCurrentBalance(data);
+      },
+      (error) => {
+        console.error('Error fetching transactions', error);
+      }
+    );
+  }
+
+  calculateCurrentBalance(transactions: any[]): number {
+    let balance = 0;
+    transactions.forEach((transaction) => {
+      if (transaction.transactionType === 'ADD_MONEY') {
+        balance += transaction.amount;
+      } else if (transaction.transactionType === 'SPEND_MONEY') {
+        balance -= transaction.amount;
+      }
+    });
+    return balance;
+  }
 }
